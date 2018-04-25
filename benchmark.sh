e2d3730aff969663f5c9f87ef6d5e5f5497a5a06
Alex Beggs <abeggs@spotify.com>
2018-04-25 13:55:51 -0400

    Reorder targets

--- benchmark.sh
+++ benchmark.sh
@@ -10,7 +10,7 @@ if ! [ -x "$(command -v bazel)" ]; then
   exit 1
 fi
 
-targets="abiChangeWithLeafLocalCache abiChangeWithRootLocalCache androidManifestChangeWithLeafLocalCache androidManifestChangeWithRootLocalCache androidResourceChangeWithLeafLocalCache androidResourceChangeWithRootLocalCache cleanWithColdLocalCache nonAbiChangeWithLeafLocalCache nonAbiChangeWithRootLocalCache"
+targets="cleanWithColdLocalCache abiChangeWithLeafLocalCache abiChangeWithRootLocalCache nonAbiChangeWithLeafLocalCache nonAbiChangeWithRootLocalCache androidManifestChangeWithLeafLocalCache androidManifestChangeWithRootLocalCache androidResourceChangeWithLeafLocalCache androidResourceChangeWithRootLocalCache"
 timestampDir=$(date +"%Y%m%d_%H%M%S")
 outputDir=output/$timestampDir
 iterations=10
